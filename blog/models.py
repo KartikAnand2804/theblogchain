1fdff2ef0d7632ae895a8cce8c6a75c5b4c78cb1
KartikAnand2804 <kartikanandwho@gmail.com>
2021-05-22 21:40:34 +0530

    ..

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from django.contrib.auth.models import User 
 
 # Create your models here.
+
+# post summary diplayed on the page index.html 
 class Post(models.Model):
 	title = models.CharField(max_length=100)
 	content = models.TextField()
@@ -11,14 +13,29 @@ class Post(models.Model):
 	def __str__(self):
 		return self.title
 
+# blog posts in all the blog pages 
 class PostPage(models.Model):
 	title = Post.title
+	author = models.CharField(max_length=100, default=User) #change it to the current logged in user
+	introduction = models.TextField(default='Type your introduction here ...')
 	content = models.TextField()
+	
+	quote = models.CharField(max_length=300, default='Quote from your blog here ...')
+	
+	titlePara1 = models.CharField(max_length=100, default='...') 
+	para1 = models.TextField(default='...')
+
+	para2 = models.TextField(default='...')
+
+	titlePara3 = models.CharField(max_length=100, default='...') 
+	para3 = models.TextField(default='...')
+	
 	date_posted = Post.date_posted
 
 	def __str__(self):
 		return self.title
 
+#contact us form in the contact.html page
 class Contact(models.Model):
 	name = models.CharField(max_length=100)
 	email = models.EmailField()
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Post, PostPage, Contact
+from django.contrib import messages
 
 
 # Create your views here.
 
+postcontent = {'content': PostPage.objects.all()} # post pages dictionary
+
 def index(request):
-	context = {'posts': Post.objects.all()
-	}
+	context = {'posts': Post.objects.all()} # post summaries dictionary
 	return render(request, 'blog/index.html', context)
 
 def about(request):
@@ -22,8 +24,21 @@ def contact(request):
 		
 		contact = Contact(name=name, email=email, website=website, message=message)
 		contact.save()
+		messages.success(request, 'Message sent!')
 
 	return render(request, 'blog/contact.html')
 
 def category(request):
 	return render(request, 'blog/category.html')
+
+def audio(request):
+	return render(request, 'blog/single-audio.html', postcontent)
+
+def gallery(request):
+	return render(request, 'blog/single-gallery.html', postcontent)
+
+def video(request):
+	return render(request, 'blog/single-video.html', postcontent)
+
+def guide(request):
+	return render(request, 'blog/style-guide.html', postcontent)
